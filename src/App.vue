89c6c56c57744efe17bd0439e0fa6f633c8be8c7
xiaofei777 <3087823110@qq.com>
2025-10-29 22:00:23 +0800

    Layout改进

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+<template>
+  <!-- 添加一级路由出口 -->
+  <router-view></router-view>
+</template>
+
+<style scoped></style>
+
+
---
+++ src/views/Category/index.vue
@@ -0,0 +1,3 @@
+<template>
+  我是Category
+</template>
