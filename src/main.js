345d348e48ebd92a7abd17d26da645540aca2291
xiaofei777 <3087823110@qq.com>
2025-10-30 22:09:57 +0800

    home-个性推荐和图片懒加载

--- src/main.js
+++ src/main.js
@@ -7,12 +7,14 @@ import router from './router'
 //引入初始化样式文件
 import '@/styles/common.scss'
 
+// 全局指令注册
+import { lazyPlugin } from "@/directives";
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
+app.use(lazyPlugin);
 app.mount('#app')
 
 
