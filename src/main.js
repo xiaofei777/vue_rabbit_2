ae749a4b1bf539d4e233d2381b7281416fa25711
xiaofei777 <3087823110@qq.com>
2025-11-02 20:39:40 +0800

    详情页-全局组件插件化

--- src/main.js
+++ src/main.js
@@ -10,11 +10,15 @@ import '@/styles/common.scss'
 // 全局指令注册
 import { lazyPlugin } from "@/directives";
 
+//引入全局组件插件
+import { componentPlugin } from './components'
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 app.use(lazyPlugin);
+app.use(componentPlugin)
 app.mount('#app')
 
 
