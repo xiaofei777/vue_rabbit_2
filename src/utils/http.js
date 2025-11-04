75e7fa9e0cb2e9f4cb4eae40af05e474780fc0e1
xiaofei777 <3087823110@qq.com>
2025-11-04 22:15:29 +0800

    登录基础业务

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,5 +1,6 @@
 //axios基础的封装
-import axios from 'axios';
+import axios from 'axios'
+import { ElMessage } from 'element-plus'
 
 // 创建axios实例
 const httpInstance = axios.create({
@@ -15,7 +16,12 @@ httpInstance.interceptors.request.use(config => {
 
 //axios响应拦截器
 httpInstance.interceptors.response.use(res => res.data, e => {
-  return Promise.reject(e)
+  //统一错误提示
+    ElMessage({
+      type: "warning",
+      message: e.response.data.message,
+    });
+    return Promise.reject(e);
 })
 
 export default httpInstance;
--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,5 +1,10 @@
 <script setup>
 import { ref } from 'vue'
+import { loginAPI } from '@/apis/user'
+import { ElMessage } from "element-plus";
+import "element-plus/theme-chalk/el-message.css";
+import { useRouter } from 'vue-router'
+
 // 表单数据对象
 const userInfo = ref({
   account: '1311111111',
@@ -39,14 +44,21 @@ const rules = {
 
 //3.获取form实例做统一校验
 const formRef = ref(null)
+const router=useRouter()
 const doLogin = () => {
-  formRef.value.validate(valid => {
+  const {account,password} = form.value
+  formRef.value.validate(async (valid) => {
     //valid:所有表单都通过校验，才为true
     console.log(valid)
     //以valid作为判断条件，如果通过校验才执行登录逻辑
     if(valid){
       //TODO 登录请求
-
+      const res=await loginAPI({account,password})
+      console.log(res)
+      //1.提示用户
+      ElMessage({ type: "success", message: "登录成功" });
+      //2.跳转首页
+      router.replace({ path: "/" });
     }
   })
 }
