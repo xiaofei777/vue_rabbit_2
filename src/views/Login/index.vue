80051993e9d4071fcb28b3b75fc358841004969e
xiaofei777 <3087823110@qq.com>
2025-11-05 16:55:30 +0800

    登录-pinia数据持久化

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,9 +1,12 @@
 <script setup>
 import { ref } from 'vue'
-import { loginAPI } from '@/apis/user'
 import { ElMessage } from "element-plus";
 import "element-plus/theme-chalk/el-message.css";
-import { useRouter } from 'vue-router'
+import { useRouter } from 'vue-router';
+import { useUserStore } from "@/stores/user.js";
+
+const userStore = useUserStore()
+
 
 // 表单数据对象
 const userInfo = ref({
@@ -53,8 +56,10 @@ const doLogin = () => {
     //以valid作为判断条件，如果通过校验才执行登录逻辑
     if(valid){
       //TODO 登录请求
-      const res=await loginAPI({account,password})
-      console.log(res)
+      await userStore.getUserInfo({
+        account,
+        password
+      })
       //1.提示用户
       ElMessage({ type: "success", message: "登录成功" });
       //2.跳转首页
